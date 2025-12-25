7925325eced11a4e0a92228a52b764f9e17d1ef4
Abdullah Arif <arifab@chalmers.se>
2025-12-25 12:45:45 +0100

    #2 Update the message Schema with Parent Message Id

--- server/models/message.model.js
+++ server/models/message.model.js
@@ -5,7 +5,12 @@ const messageSchema = new Schema({
   messageId: { type: String, unique: true, required: true, immutable: true, index:true },
   Body: {type: String, required: true },
   SendTimestamp: { type: Date }, 
-  Reaction: { type: String, enum: ['👍', '❤️', '😂', '😢', '😡', null] }, 
+  ParentMessageId:{type: Schema.Types.ObjectId , ref: 'messages', required: false},
+  Reactions: [{
+    userId: { type: Schema.Types.ObjectId, ref: 'User' },
+    reaction: { type: String, enum: ['👍', '❤️', '😂', '😢', '😡'] },
+    timestamp: { type: Date, default: Date.now , required: false},
+  }],
   ResponseIds: [{ type: mongoose.Schema.ObjectId, ref: 'messages', required: false }], 
   //Foreign key Refferences
   Sender: { type: Schema.Types.ObjectId, ref: 'User', required: true }, 
