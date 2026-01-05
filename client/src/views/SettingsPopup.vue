f00320b9332f75c3c2ae7d20dd3952ea081d1c3a
Sireen Abu Ajamieh <ajamieh@student.chalmers.se>
2026-01-05 14:16:46 +0100

    #29 Refactor navigation and add bootstrap

--- client/src/views/SettingsPopup.vue
+++ client/src/views/SettingsPopup.vue
@@ -3,9 +3,9 @@
     <div class="card">
       <h2>Settings</h2>
 
-      <button @click="main">Main Page</button>
-      <button @click="logout">Log out</button>
-      <button @click="cancel">Cancel</button>
+      <b-button @click="main">Main Page</b-button>
+      <b-button @click="logout">Log out</b-button>
+      <b-button @click="cancel">Cancel</b-button>
     </div>
   </div>
 </template>
@@ -28,35 +28,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.settings {
-  position: fixed;
-  background: rgba(0,0,0,0.4);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-}
-
-.card {
-  background: #6d2a46;
-  padding: 24px;
-  width: 280px;
-  border-radius: 28px;
-  text-align: center;
-  color: #fadde8;
-}
-
-.card button {
-  width: 100%;
-  margin-top: 10px;
-  padding: 10px;
-  border-radius: 999px;
-  border: none;
-  cursor: pointer;
-  background: #fadde8;
-}
-</style>
+<style scoped>@import "../style/NavigatePage.css";</style>
