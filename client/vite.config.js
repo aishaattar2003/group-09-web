5f5af22b2a9d516a52c5632c88a79f607f422e11
Aisha Attar <aishaat@student.chalmers.se>
2026-01-06 20:35:15 +0100

    Add render config for testing purposes

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,7 @@ import { BootstrapVueNextResolver } from 'bootstrap-vue-next'
 
 // https://vitejs.dev/config /
 export default defineConfig({
+  base: '/group-09-web/',
   plugins: [
     vue(),
     Components({
